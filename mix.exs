f3b9baff29ca781b4b65fdb0927d5c17b229a735
Youssif Salama <youssifsalama@gmail.com>
2024-02-10 22:23:48 -0500

    torchx compatibility

--- mix.exs
+++ mix.exs
@@ -24,7 +24,8 @@ defmodule TheBrain.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:bumblebee, "~> 0.4.2"},
-      {:exla, ">= 0.0.0"}
+      #{:exla, ">= 0.0.0"},
+      {:torchx, "~> 0.6"}
     ]
   end
 end
